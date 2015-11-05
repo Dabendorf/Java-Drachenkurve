5081b9b4325e11a6ede5c0bab270e407ac797c56
Lukas Schramm <schramm98@icloud.com>
2015-11-05 20:02:22 +0100

    Hinzufuegen erster Funktionalitaet
    Diese Methode fuegt einen funktionierenden Drachenkurvegenerator hinzu,
    welcher noch graphisch aufgearbeitet werden muss.

---
+++ src/drachenkurve/Drachenkurve.java
@@ -0,0 +1,55 @@
+package drachenkurve;
+
+import java.util.ArrayList;
+
+/**
+ * Dies ist die Klasse des Projekts Drachenkurve, welche die Schritte der Kurve fuer eine bestimmte Laenge berechnet.
+ * 
+ * @author Lukas Schramm
+ * @version 1.0
+ *
+ */
+public class Drachenkurve {
+	
+	/**Liste der Himmelsrichtungen*/
+	private ArrayList<Character> directions = new ArrayList<Character>();
+	
+	public Drachenkurve(int gene) {
+		directions.add('o');
+		for(int i=0;i<gene;i++) {
+			nextGeneration();
+			System.out.println(directions);
+		}
+	}
+	
+	/**
+	 * Diese Methode macht aus der aktuellen Drachenkurve die naechste Generation einer Drachenkurve.
+	 */
+	private void nextGeneration() {
+		for(int i=0;i<directions.size();i++) {
+			switch(directions.get(i)) {
+			case 'o':
+				directions.set(i, 'o');
+				directions.add(i+1,'n');
+				break;
+			case 'n':
+				directions.set(i, 'w');
+				directions.add(i+1,'n');
+				break;
+			case 'w':
+				directions.set(i, 'w');
+				directions.add(i+1,'s');
+				break;
+			case 's':
+				directions.set(i, 'o');
+				directions.add(i+1,'s');
+				break;
+			}
+			i++;
+		}
+	}
+
+	public ArrayList<Character> getDirections() {
+		return directions;
+	}
+}
